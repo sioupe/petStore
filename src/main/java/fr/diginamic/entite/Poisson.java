7b4c503c9c4e78dadbbdc9fd25abf1b91d0c3dcf
johan guillen <sioupes34@gmail.com>
2024-11-07 18:55:15 +0100

    eciture de la javadoc

--- src/main/java/fr/diginamic/entite/Poisson.java
+++ src/main/java/fr/diginamic/entite/Poisson.java
@@ -6,10 +6,14 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "poisson")
 public class Poisson extends Animale{
+    /**lieu de vie du poisson*/
     @Enumerated(EnumType.STRING)
     @Column(name ="FISH_LIVING_ENV")
     private FishLivEnv livingEnv;
 
+    /**
+     * contructeur vide
+     */
     public Poisson() {
         super();
     }
